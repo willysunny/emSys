593c10da55b7ab5e0f7250de35c24eaf297cfa39
Willy_Sunny <willy_sunny@ts3chinese.com>
2016-12-05 00:35:52 +0800

    User Management - Init Push

---
+++ fujunEmSys/system/pnlUserManage.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class pnlUserManage
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MetroListView1 = New MetroFramework.Controls.MetroListView()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MetroListView1
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.MetroListView1, 3)
+        Me.MetroListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MetroListView1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.MetroListView1.FullRowSelect = True
+        Me.MetroListView1.Location = New System.Drawing.Point(3, 3)
+        Me.MetroListView1.Name = "MetroListView1"
+        Me.MetroListView1.OwnerDraw = True
+        Me.MetroListView1.Size = New System.Drawing.Size(773, 512)
+        Me.MetroListView1.TabIndex = 0
+        Me.MetroListView1.UseCompatibleStateImageBehavior = False
+        Me.MetroListView1.UseSelectable = True
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334!))
+        Me.TableLayoutPanel1.Controls.Add(Me.MetroListView1, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(779, 598)
+        Me.TableLayoutPanel1.TabIndex = 1
+        '
+        'pnlUserManage
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "pnlUserManage"
+        Me.Size = New System.Drawing.Size(779, 598)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MetroListView1 As MetroFramework.Controls.MetroListView
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+End Class
---
+++ fujunEmSys/system/pnlUserManage.vb
@@ -0,0 +1,3 @@
+﻿Public Class pnlUserManage
+
+End Class
